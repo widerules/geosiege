f6717719885f7c19f2d3cc4355967e41ccbaa299
scott@zeddic.com <scott@zeddic.com@65fa9b40-ce77-e322-4b1c-cc03632e9828>
2010-07-05 22:32:42 +0000

    Minor comments and flags added to the base GameObject class.

--- trunk/src/com/geosiege/common/GameObject.java
+++ trunk/src/com/geosiege/common/GameObject.java
@@ -25,8 +25,14 @@ import android.graphics.Canvas;
 
 public class GameObject {
   
+  /** Whether an object should be actively drawn and updated. */
   public boolean active = true;
+  
+  /** Whether an object can be restored to an object pool. */
   public boolean canRecycle = false;
+  
+  /** Whether an object was obtained from an object pool. */
+  public boolean taken = false;
   
   ArrayList<Component> components;
    
@@ -66,6 +72,9 @@ public class GameObject {
     canRecycle = false;
   }
   
+  /**
+   * Disables an object and clears it to be restored to an object pool.
+   */
   public void kill() {
     active = false;
     canRecycle = true;
