371700a31eac9a91c270d02de652fd998b625343
scott@zeddic.com <scott@zeddic.com@65fa9b40-ce77-e322-4b1c-cc03632e9828>
2010-08-16 20:22:16 +0000

    Add new activities / game harness that uses the default android UI elements rather than the hacked together home brew one.

---
+++ trunk/src/com/geosiege/game/MenuLevel.java
@@ -0,0 +1,38 @@
+package com.geosiege.game;
+
+import android.os.Bundle;
+
+import com.geosiege.game.Levels.Difficulty;
+
+public class MenuLevel {
+ 
+  private static final String SELECTED_LEVEL_BUNDLE_KEY = "level";
+  
+  public String file;
+  public String name;
+  public Difficulty difficulty;
+  public boolean locked;
+  
+  public MenuLevel(String file, String name, Difficulty difficulty, boolean locked) {
+    this.file = file;
+    this.name = name;
+    this.locked = locked;
+    this.difficulty = difficulty;
+  }
+  
+  public int getLevelIconResource() {
+    return Levels.getDifficultyIcon(difficulty);
+  }
+  
+  public void storeInBundle(Bundle bundle) {
+    bundle.putString(SELECTED_LEVEL_BUNDLE_KEY, name);
+  }
+  
+  public static final MenuLevel getLevelFromBundle(Bundle bundle) {
+    if (!bundle.containsKey(SELECTED_LEVEL_BUNDLE_KEY)) {
+      throw new RuntimeException("Bundle didn't contain level information.");
+    }
+    
+    return Levels.getLevelWithName(bundle.getString(SELECTED_LEVEL_BUNDLE_KEY));
+  }
+}
